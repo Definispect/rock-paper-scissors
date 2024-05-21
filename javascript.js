7acf03f15c6bf938e5a49e385f97d2bdca3b339a
Definispect <153238434+Definispect@users.noreply.github.com>
2024-05-21 18:12:13 -0400

    Generate random number
    Function for generating computer choice defined and tested

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Testing 123")
+function getComputerChoice() {
+    let choiceNumber = Math.floor(Math.random()* 100) ;
+    console.log(choiceNumber); //debug
+
+    if (choiceNumber <= 33) {
+        console.log("rock"); //debug
+        return 1;
+    }
+    if (choiceNumber > 33 && choiceNumber < 66) {
+        console.log("paper"); //debug
+        return 2;
+    }
+    if (choiceNumber >= 66) {
+        console.log("scissors"); //debug
+        return 3;
+    }
+}
+
+getComputerChoice();
