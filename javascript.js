ec60a379887c92b36146f1ff64044685de223033
Definispect <153238434+Definispect@users.noreply.github.com>
2024-05-21 22:25:32 -0400

    Create user input function
    Allow user to input valid text to select move, condense computer func
          console.log(humanInput); //debug

--- javascript.js
+++ javascript.js
@@ -1,19 +1,41 @@
 function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random()* 100) ;
-    console.log(choiceNumber); //debug
 
     if (choiceNumber <= 33) {
-        console.log("rock"); //debug
-        return 1;
+        return "rock";
     }
     if (choiceNumber > 33 && choiceNumber < 66) {
-        console.log("paper"); //debug
-        return 2;
+        return "paper";
     }
     if (choiceNumber >= 66) {
-        console.log("scissors"); //debug
-        return 3;
+        return "scissors";
     }
 }
 
+function getHumanChoice() {
+    while (true) {
+        let humanInput= prompt("1. Rock, 2. Paper, or 3. Scissors?", "Pick a number (1,2,3) or type in the word");
+
+        if (humanInput == null) {return null;}
+
+        humanInput = humanInput.toLowerCase()
+
+        if (humanInput == "rock" || humanInput == 1) {
+            console.log('You chose Rock');
+            return "rock";
+        }
+        if (humanInput == "paper" || humanInput == 2) {
+            console.log("You chose Paper");
+            return "paper";
+        }
+        if (humanInput == "scissors" || humanInput == 3) {
+            console.log("You chose Scissors");
+            return "scissors";
+        }
+
+        alert(`${humanInput} is not a valid choice. Please choose again.`)
+    }   
+}
+
 getComputerChoice();
+getHumanChoice();
